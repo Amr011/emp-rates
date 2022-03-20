0cd69a9d834805c4dc72f89c07a8cc17bd40c8af
Amr109 <grand.109.master@gmail.com>
2022-03-20 03:19:27 +0200

    dashboard update

--- services/index.js
+++ services/index.js
@@ -1,3 +1,7 @@
-$(function () {
-  $('#datepicker').datepicker()
+var menu_btn = document.querySelector('#menu-btn')
+var sidebar = document.querySelector('#sidebar')
+var container = document.querySelector('.my-container')
+menu_btn.addEventListener('click', () => {
+  sidebar.classList.toggle('active-nav')
+  container.classList.toggle('active-cont')
 })
